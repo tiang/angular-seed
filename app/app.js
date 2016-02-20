da21abc3e102f2aa77095e00542986d8c06643de
Tiang Cheng <tiang_cheng@mckinsey.com>
2016-02-20 14:54:21 +1100

    ui router migration

--- app/app.js
+++ app/app.js
@@ -1,17 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
-  })
-  .otherwise({redirectTo: '/view1'});
-  
+angular.module('myApp', ['ui.router'])
+.config(['$stateProvider', function($stateProvider) {
+
 }]);
