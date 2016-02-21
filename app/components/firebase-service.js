8d8f7877f55e2e0c5c7dcbc3c25e13a9d933ffce
Tiang Cheng <tiang_cheng@mckinsey.com>
2016-02-21 11:07:03 +1100

    firebase title experiment

--- app/components/firebase-service.js
+++ app/components/firebase-service.js
@@ -1,9 +1,42 @@
 angular.module('myApp')
 .factory('FirebaseService', function() {
+    var serviceTitle;
+    var getTitleInServiceMethod = function() {
+                        if (this.serviceTitle == null)
+                            return "No title set"    
+                        else 
+                            return this.serviceTitle;
+                        };
+                
     return {
-        firebaseURL: "https://ngfeb.firebaseio.com"
+        firebaseURL: "https://ngfeb.firebaseio.com",
+        setTitle: function(controllerTitle) {
+            this.serviceTitle = controllerTitle;
+        },
+        getTitle: getTitleInServiceMethod,
+        getTitle2: this.serviceTitle
     }
 })
-.controller('ChatAppController', function($scope) {
+.controller('ChatAppController', function($scope,FirebaseService) {
     $scope.title= "Hello Chat App"
+    $scope.url = FirebaseService.firebaseURL;
+    console.log(FirebaseService.getTitle());
+    FirebaseService.setTitle('My New Title')
+    console.log(FirebaseService.getTitle());
 });
+
+
+/****  App Route  ****/
+// .state('chat', {
+//     url: "/chat",
+//     templateUrl: "partials/chat.html",
+//     controller: 'ChatAppController'
+// });
+
+
+/****  HTML  ****/
+// Hello World
+
+// {{title}}
+// <br>
+// {{url}}
