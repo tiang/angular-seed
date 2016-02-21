129f2e8768be8d7d1377d3f2c357fd2048252048
Tiang Cheng <tiang_cheng@mckinsey.com>
2016-02-21 12:04:27 +1100

    added angular firebase library

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', ['ui.router'])
+angular.module('myApp', ['ui.router', 'firebase'])
 .config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /state1
